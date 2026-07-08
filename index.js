34bc984ee6f0f3ec4ce4af1130b309839e963a1b
Ronish Ghimire <iamrg2060@gmail.com>
2026-07-08 23:57:08 +0545

    Add GitHub Actions workflow for deploying NodeJS application and update greeting message

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json({ message: "Hello, World!" });
+    res.json({ message: "Hello, World!!!" });
 });
 
 app.listen(PORT, () => {
